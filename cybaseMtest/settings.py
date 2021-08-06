1c0305abb1029444c9e1a8e793d8c3dcb2d80cdd
Saleem Malik <saleemalik13@gmail.com>
2021-08-06 15:58:45 +0530

    deploying the app

--- cybaseMtest/settings.py
+++ cybaseMtest/settings.py
@@ -78,12 +78,9 @@ WSGI_APPLICATION = 'cybaseMtest.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES = {}
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators

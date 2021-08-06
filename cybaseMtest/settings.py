fb1d613c9af13fadef6c478480cf5443c92bf689
Saleem Malik <saleemalik13@gmail.com>
2021-08-06 14:06:41 +0530

    Databasemigration

--- cybaseMtest/settings.py
+++ cybaseMtest/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@l#c(y(%v@2#)p-z^kc*ajnmemp*+yysnm^b(axho883c4jze^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','cybase-machinetst.herokuapp.com']
 

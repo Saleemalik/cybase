9eeccec3cb6f3fcb073d9d877cdd7521f901e096
Saleem Malik <saleemalik13@gmail.com>
2021-08-06 14:41:28 +0530

    adding changes

--- cybaseMtest/settings.py
+++ cybaseMtest/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@l#c(y(%v@2#)p-z^kc*ajnmemp*+yysnm^b(axho883c4jze^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','cybase-machinetst.herokuapp.com']
 
